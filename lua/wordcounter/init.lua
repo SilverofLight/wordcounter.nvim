71d567335522afdd3a57a4934373e93fbdca9adb
npc-z <1763998996@qq.com>
2023-10-18 14:42:15 +0800

    recount when buffer changed otherwise read caches

--- lua/wordcounter/init.lua
+++ lua/wordcounter/init.lua
@@ -34,6 +34,17 @@ end
 M.count_buf_words = function(buf_no)
     local lines = vim.api.nvim_buf_get_lines(buf_no, 0, -1, false)
     local name = vim.api.nvim_buf_get_name(buf_no)
+
+    local bufno = "%"
+    if buf_no ~= 0 then
+        bufno = buf_no
+    end
+    local changed = util.is_buf_changed(bufno)
+
+    if not changed and util.contains_key(M._bufs, name) then
+        return M._bufs[name]
+    end
+
     local words_count = 0
 
     local w = M.count_lines_words(lines)
--- lua/wordcounter/util.lua
+++ lua/wordcounter/util.lua
@@ -7,38 +7,50 @@ end
 local M = {}
 
 local chinese_punctuations = {
-    '，',
-    '。',
-    '？',
-    '《',
-    '》',
-    '（',
-    '）',
-    '【',
-    '】',
-    '「',
-    '」',
-    '『',
-    '』',
-    '❲',
-    '❳',
-    '［',
-    '］',
-    '、',
-    '|',
-    '；',
-    '：',
-    '“',
-    '”',
-    "",
-    '',
-    '—',
-    '…',
-    '！',
-    '～',
-    '`',
+    "，",
+    "。",
+    "？",
+    "《",
+    "》",
+    "（",
+    "）",
+    "【",
+    "】",
+    "「",
+    "」",
+    "『",
+    "』",
+    "❲",
+    "❳",
+    "［",
+    "］",
+    "、",
+    "|",
+    "；",
+    "：",
+    "“",
+    "”",
+    "—",
+    "…",
+    "！",
+    "～",
 }
 
+local function now()
+    return os.date('%Y-%m-%d %H:%M:%S')
+end
+
+---@type fun(tbl: table, key: string): boolean
+local contains_key = function(tbl, key)
+    for k, _ in pairs(tbl) do
+        if k == key then
+            return true
+        end
+    end
+    return false
+end
+
+---@type fun(list: table, element: string): boolean
 local contains = function(list, element)
     for _, e in pairs(list) do
         if e == element then
@@ -48,13 +60,14 @@ local contains = function(list, element)
     return false
 end
 
+---@type fun(char: string): boolean
 local is_chinese_punctuation = function(char)
     return contains(chinese_punctuations, char)
 end
 
 -- inspect the value
 local P = function(value)
-    print(vim.inspect(value))
+    print(now(), vim.inspect(value))
     return value
 end
 
@@ -71,9 +84,15 @@ local function get_visual_selection()
     end
 end
 
+local function is_buf_changed(bufno)
+    return vim.fn.getbufinfo(bufno)[1].changed == 1
+end
+
 M.is_chinese_punctuation = is_chinese_punctuation
 M.get_visual_selection = get_visual_selection
 M.contains = contains
+M.contains_key = contains_key
+M.is_buf_changed = is_buf_changed
 M.P = P
 M.utf8 = utf8
 
